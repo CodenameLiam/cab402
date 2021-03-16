c28d9d3c5f12a5f3a574d5ec0541ac791d8f2c0f
Liam Percy <liampercy123@gmail.com>
2021-03-16 16:13:49 +1000

    HOFs

---
+++ Lecture 3/Lists.fs
@@ -0,0 +1,12 @@
+﻿module Lists
+
+let rec length L =
+    match L with
+    | [] -> 0
+    | head::tail -> 1 + (length tail)
+
+
+let rec reverse L =
+    match L with
+    | [] -> []
+    | head::tail -> (reverse tail) @ [head]
---
+++ Lecture 3/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+
+    let list = [1;2;3]
+    let length = Lists.length(list)
+    let reverse = Lists.reverse(list)
+
+    let minimum = List.map (fun x -> x * 2) list |> List.reduce (fun x y -> if x < y then x else y)
+    let evens = List.filter (fun x -> if x % 2 = 0 then true else false) list
+
+
+
+    0 // return an integer exit code
