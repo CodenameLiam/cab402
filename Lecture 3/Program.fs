13d373b2217c6fc7927e4733cfbeb380f0d93b4f
Liam Percy <liampercy123@gmail.com>
2021-03-16 17:22:45 +1000

    Lecture 3

--- Lecture 3/Program.fs
+++ Lecture 3/Program.fs
@@ -5,12 +5,17 @@ open System
 [<EntryPoint>]
 let main argv =
 
-    let list = [1;2;3]
+    let list = [5;3;2;2;3]
+
     let length = Lists.length(list)
     let reverse = Lists.reverse(list)
 
     let minimum = List.map (fun x -> x * 2) list |> List.reduce (fun x y -> if x < y then x else y)
     let evens = List.filter (fun x -> if x % 2 = 0 then true else false) list
+
+    let mapreduce m r = (List.map m) >> (List.reduce r)
+    let minimumUsingCustomMR = mapreduce (fun x -> x * 2) (fun x y -> if x < y then x else y) list
+
 
 
 
