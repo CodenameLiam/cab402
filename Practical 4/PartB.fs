2b40f429cab5ea5dce2da7605fb88d3d2550dddc
Liam Percy <liampercy123@gmail.com>
2021-03-24 16:33:17 +1000

    Practical 4

---
+++ Practical 4/PartB.fs
@@ -0,0 +1,15 @@
+﻿module PartB
+
+let B1 = 
+    let getRandom a = 
+        let random = new System.Random(a)
+        random.Next(1, 100)
+           
+
+    let r1 = getRandom 2
+    let r2 = getRandom 6
+
+    printfn "Random number 1: %d" r1
+    printfn "Random number 2: %d" r2
+
+    
--- Practical 4/Program.fs
+++ Practical 4/Program.fs
@@ -5,7 +5,17 @@ open System
 [<EntryPoint>]
 let main argv =
     
-    PartA.A3
+    //PartA.A3
+
+    //PartB.B1
+
+    PartC.C
+
+
+
+   
+
+    
     
     // printfn "Hello World from F#!"
     0 // return an integer exit code
